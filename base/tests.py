a05ea744d1188565dbba0ddb90fa7f7d9429df1c
shirannimni <shirannimni@gmail.com>
2024-06-27 19:31:24 +0300

    edit tests.py

--- base/tests.py
+++ base/tests.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,17 +10,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 class TestWebApp:
     def setup_method(self):
-        # Set the path to the chromedriver executable
         chrome_options = Options()
-        chrome_options.add_argument("--headless")  # Run in headless mode (no GUI)
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920,1080")    
+        chrome_options.add_argument("--remote-debugging-port=9222")
+    
+        service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
         #chrome_options.add_argument("--disable-extensions")
         #chrome_options.add_argument("--ignor-certificate-errors")
         #chrome_options.add_argument("--test-type")
-        chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.add_argument("--remote-debugging-port=9222")
+        
 
         #chrome_options.binary_location = "/opt/google/chrome/chrome"
 
@@ -34,11 +36,8 @@ class TestWebApp:
         #                               desired_capabilities=capabilities)
         #self.driver.set_page_load_timeout(20)
          
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=chrome_options)
-        request.cls.driver = driver
-        yield
-        driver.quit()
+        
+
         
 
        # try:
