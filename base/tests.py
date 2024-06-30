8d35fbcb394d8df564699c324b4dfcf3895e1c77
shirannimni <shirannimni@gmail.com>
2024-06-30 22:17:49 +0300

    remove head

--- base/tests.py
+++ base/tests.py
@@ -5,12 +5,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 
 class TestWebApp:
     def setup_method(self):
         # Set the path to the chromedriver executable
         chrome_driver_path = "/usr/bin/google-chrome"
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver = webdriver.Chrome(options=options,service=Service(ChromeDriverManager().install()))
 
     def teardown_method(self):
         self.driver.quit()
